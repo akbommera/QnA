7770e8060330a9be2eef240e68c07d1b088a9b14
akshaykumar <akbommera@gmail.com>
2019-12-26 15:10:02 +0530

    Added new cloud service for mongodb

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import * as mongoose from "mongoose";
 
-const uri: string = "mongodb://127.0.0.1:27017/qna";
+const uri: string = "mongodb+srv://admin:nab24259495@akbcluster007-opgas.mongodb.net/qna?retryWrites=true&w=majority";
 
 mongoose.connect(uri, { useNewUrlParser: true }, (err: any) => {
     if (err) {
